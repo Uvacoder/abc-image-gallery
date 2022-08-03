cb8108f94628e709286194aa56b083d7c267155f
peltonan <peltonen.annette@gmail.com>
2022-08-03 16:05:48 +0300

    Messing along, trying to find a gallery style I like

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "photoswipe/dist/photoswipe.css";
+import "photoswipe/dist/default-skin/default-skin.css";
+
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Gallery>
+      <Item
+        original="https://picsum.photos/1024/768?image=2"
+        thumbnail="https://picsum.photos/200/300?image=2"
+        width="1024"
+        height="768"
+      >
+        {({ ref, open }) => (
+          <img
+            ref={ref}
+            onClick={open}
+            src="https://picsum.photos/200/300?image=2"
+          />
+        )}
+      </Item>
+    </Gallery>
   );
 }
 
---
+++ src/data/images.js
@@ -0,0 +1,13 @@
+import axolotl from './images/axolotl.webp'
+
+
+export const data = [
+    {
+        polaroid: axolotl,
+        caption: "http://placeimg.com/640/360/any"
+    },
+    {
+        polaroid: "Description of image is here image2",
+        caption: "http://placeimg.com/640/360/animals"
+    }
+]
