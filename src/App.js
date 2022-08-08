66337aea2c38620e187895efee6a62f10433fa1b
peltonan <peltonen.annette@gmail.com>
2022-08-08 11:44:24 +0300

    building gallery

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import Gallery from "react-photo-gallery";
-import './App.css'
-import { photos } from "./components/Photos";
+import './App.css';
+import { cards } from './components/Cards.js'
+import { useEffect } from 'react';
 
-const App = () => {
+function App() {
+
+  useEffect(() => {
+    document.title = "Musu Gallery"
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Inktober 2022</h1>
-      <h2>Musu Gallery</h2>
-      <div className='wrapper'>
-        <Gallery photos={photos} />;
-      </div>
-    </div >
-  );
-};
 
+    <div className="App">
+      <header className="App-header">
+        <h1>Musu Gallery</h1>
+        <h2>Inktober 2022</h2>
+        <div className="wrapper">
+          {cards.map((card) => {
+            return (
+              <ul className="grid">
+                <li>
+                  <div class="grid__figure">
+                    <div className="image">
+                      <img src={card.image} />
+                      <div className="caption">{card.caption}
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            )
+          })}
+
+        </div>
+      </header>
+      <footer>
+        <div className="center">
+          <span>Disclaimer: This site is for personal use only</span>
+        </div>
+      </footer>
+    </div>
+
+  );
+}
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,183 @@
+import axolotl from '../images/axolotl.webp'
+import llama from '../images/llama.webp'
+import cats from '../images/cats.webp'
+import bees from '../images/bees.webp'
+import foxes from '../images/foxes.webp'
+import sheep from '../images/sheep.webp'
+import turtle from '../images/turtle.webp'
+import dog from '../images/dog.webp'
+import parrots from '../images/parrots.avif'
+import dolphin from '../images/dolphin.webp'
+import fish from '../images/fish.avif'
+import dyes from '../images/dyes.avif'
+import glazedterracotta from '../images/glazedterracotta.webp'
+import terracotta from '../images/terracotta.png'
+import concrete from '../images/concrete.jpg'
+import glass from '../images/glass.jpg'
+import flowers from '../images/flowers.avif'
+import sporeblossom from '../images/sporeblossom.avif'
+import azalea from '../images/azalea.webp'
+import candles from '../images/candles.jpg'
+import glowberries from '../images/glowberries.jpg'
+import glowsquid from '../images/glowsquid.webp'
+import subwoolfer from '../images/subwoolfer.jpg'
+
+export const cards = [
+
+    {
+        /* Day 1 */
+        image: axolotl,
+        caption: "Find all 5 Axolotls",
+    },
+    {
+        /* Day 2 */
+        image: llama,
+        caption: "Get all 16 Llamas",
+    },
+    {
+        /* Day 3 */
+        image: cats,
+        caption: "Get all 10 Cats",
+    },
+    {
+        /* Day 4 */
+        image: bees,
+        caption: "Have Bees",
+    },
+    {
+        /* Day 5 */
+        image: foxes,
+        caption: "Have Foxes",
+    },
+    {
+        /* Day 6 */
+        image: sheep,
+        caption: "Get all 16 Sheep",
+    },
+    {
+        /* Day 7 */
+        image: turtle,
+        caption: "Have Turtles",
+    },
+    {
+        /* Day 8 */
+        image: dog,
+        caption: "Have all 16 Dogs",
+    },
+    {
+        /* Day 9 */
+        image: parrots,
+        caption: "Get all 5 Parrots",
+    },
+    {
+        /* Day 10 */
+        image: dolphin,
+        caption: "Have Dolphins",
+    },
+    {
+        /* Day 11 */
+        image: fish,
+        caption: "Get all 22 Tropical Fish",
+    },
+    {
+        /* Day 12 */
+        image: dyes,
+        caption: "Get all 16 Dyes",
+    },
+    {
+        /* Day 13 */
+        image: glazedterracotta,
+        caption: "Get all 16 Glazed Terracotta",
+    },
+    {
+        /* Day 14 */
+        image: terracotta,
+        caption: "Get all 17 Terrcotta",
+    },
+    {
+        /* Day 15 */
+        image: concrete,
+        caption: "Get all 16 Concrete",
+    },
+    {
+        /* Day 16 */
+        image: glass,
+        caption: "Get all 17 Glass",
+    },
+    {
+        /* Day 17 */
+        image: flowers,
+        caption: "Get all 14 Flowers",
+    },
+    {
+        /* Day 18 */
+        image: sporeblossom,
+        caption: "Find a Spore Blossom",
+    },
+    {
+        /* Day 19 */
+        image: azalea,
+        caption: "Find Azalea Tree",
+    },
+    {
+        /* Day 20 */
+        image: candles,
+        caption: "Make all 17 Candles",
+    },
+    {
+        /* Day 21 */
+        image: glowberries,
+        caption: "Find Glowberries",
+    },
+    {
+        /* Day 22 */
+        image: glowsquid,
+        caption: "Find Glowsquids",
+    },
+    {
+        /* Day 23 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 24 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 25 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 26 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 27 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 28 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 29 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 30 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+    {
+        /* Day 31 */
+        image: subwoolfer,
+        caption: "Make a Subwoolfer Tower",
+    },
+
+]
