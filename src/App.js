c8ff4c43c6a37966cd0207ce078a7541989ac82d
peltonan <peltonen.annette@gmail.com>
2022-08-10 14:03:05 +0300

    Added wrapper

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { cards } from './components/Cards.js'
 import { useEffect } from 'react';
 
+
 function App() {
 
   useEffect(() => {
@@ -15,21 +16,24 @@ function App() {
         <h1>Musu Gallery</h1>
         <h2>Inktober 2022</h2>
       </header>
-      {cards.map((card) => {
-        return (
-          <div className='gallery-image'>
-            <div className='img-box'>
-              <img src={card.image} />
-              <div className='transparent-box'>
-                <div className="caption">{card.caption}
+      <div className='wrapper'>
+        {cards.map((card) => {
+          return (
+
+            <div className='gallery-image'>
+              <div className='img-box'>
+                <img src={card.image} />
+                <div className='transparent-box'>
+                  <div className="caption">{card.caption}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-        )
-      })}
 
+          )
+        })}
+      </div>
 
 
       <footer>
